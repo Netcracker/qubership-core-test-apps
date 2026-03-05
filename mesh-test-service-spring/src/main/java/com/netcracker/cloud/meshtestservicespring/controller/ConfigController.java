d7438fab9b783b08e7eb1460068e83e173bbf70d
Evgenii Stetsenko <i@estetsenko.com>
2026-03-05 17:08:44 +0300

    feat: add consul property source

---
+++ mesh-test-service-spring/src/main/java/com/netcracker/cloud/meshtestservicespring/controller/ConfigController.java
@@ -0,0 +1,25 @@
+package com.netcracker.cloud.meshtestservicespring.controller;
+
+import com.netcracker.cloud.meshtestservicespring.configuration.ApiVersions;
+import com.netcracker.cloud.routesregistration.common.spring.gateway.route.annotation.GatewayRequestMapping;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(ApiVersions.API + ApiVersions.V1 + "/config")
+@GatewayRequestMapping(path = ApiVersions.API + ApiVersions.V1 + ApiVersions.SERVICE_NAME + "/config")
+@Slf4j
+public class ConfigController {
+
+    @Value("${consul.test.property}")
+    private String consulTestProperty;
+
+    @RequestMapping(method = RequestMethod.GET)
+    public ResponseEntity<String> config() {
+        return ResponseEntity.ok(consulTestProperty);
+    }
+}
