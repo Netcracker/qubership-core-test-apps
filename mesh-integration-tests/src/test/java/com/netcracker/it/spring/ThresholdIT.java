26935965da50d1e6c9e8bbace2105c97ec7e6f35
alsergs <sergei.s.aleksandrov.qubership@gmail.com>
2026-07-28 16:41:52 +0300

    fix: switch circuit breaker tests to composite gateway

--- mesh-integration-tests/src/test/java/com/netcracker/it/spring/ThresholdIT.java
+++ mesh-integration-tests/src/test/java/com/netcracker/it/spring/ThresholdIT.java
@@ -1,46 +1,39 @@
 package com.netcracker.it.spring;
 
 import com.netcracker.cloud.junit.cloudcore.extension.annotations.*;
-import com.netcracker.cloud.junit.cloudcore.extension.service.PortForwardService;
-
 import lombok.extern.slf4j.Slf4j;
-import okhttp3.Request;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.*;
-import java.util.stream.Collectors;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 import static com.netcracker.it.spring.Const.*;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @Slf4j
 @EnableExtension
 @SmokeTest
-public class ThresholdIT {
-
-	@PortForward(serviceName = @Value(INTERNAL_GW_SERVICE_NAME), cloud = @Cloud(namespace = @Value(prop = ORIGIN_NAMESPACE_ENV_NAME)))
-    private static URL internalGWServerUrl;
-
-	@PortForward(serviceName = @Value(PUBLIC_GW_SERVICE_NAME), cloud = @Cloud(namespace = @Value(prop = ORIGIN_NAMESPACE_ENV_NAME)))
-    private URL publicGatewayUrl;
-
-	@PortForward(serviceName = @Value("egress-gateway"), cloud = @Cloud(namespace = @Value(prop = ORIGIN_NAMESPACE_ENV_NAME)))
-    private URL egressGatewayUrl;
+class ThresholdIT {
 
 	@PortForward(serviceName = @Value(SERVICE_NAME), cloud = @Cloud(namespace = @Value(prop = ORIGIN_NAMESPACE_ENV_NAME)))
-    private static URL compositeGWServerUrl;
+    private static URL springCompositeGWServerUrl;
 
 	@PortForward(serviceName = @Value(QUARKUS_SERVICE_NAME), cloud = @Cloud(namespace = @Value(prop = ORIGIN_NAMESPACE_ENV_NAME)))
     private static URL quarkusCompositeGWServerUrl;
 
-	@Cloud
-    static PortForwardService portForwardService;
+    @PortForward(serviceName = @Value("egress-gateway"), cloud = @Cloud(namespace = @Value(prop = ORIGIN_NAMESPACE_ENV_NAME)))
+    private URL egressGatewayUrl;
 
     private static final int REQUESTS_NUMBER = 6;
     private static final int LIMIT_VALUE = 2;
@@ -50,27 +43,26 @@ public class ThresholdIT {
     private static final int TIMEOUT_SECONDS = 10;
 
     @BeforeAll
-    public void init() throws Exception {
-        assertNotNull(publicGatewayUrl);
-        assertNotNull(internalGWServerUrl);
+    void init() {
+        assertNotNull(springCompositeGWServerUrl);
+        assertNotNull(quarkusCompositeGWServerUrl);
         assertNotNull(egressGatewayUrl);
-        assertNotNull(compositeGWServerUrl);
     }
 
     @Test
-    public void testLoadEnvoyWithConcurrency1AndConnectionLimit() throws Exception {
+    void testLoadEnvoyWithConcurrency1AndConnectionLimit() throws Exception {
         //load envoy with concurrency=1 and connection limited cluster
-        loadEnvoy(egressGatewayUrl.toString() + "api/v1/mesh-test-service-spring/sleep?seconds=" + SLEEP_DURATION_SECONDS_STRING, true);
+        loadEnvoy(springCompositeGWServerUrl.toString() + "api/v1/mesh-test-service-spring/sleep?seconds=" + SLEEP_DURATION_SECONDS_STRING, true);
     }
 
     @Test
-    public void testLoadEnvoyWithAverageConfig() throws Exception {
+    void testLoadEnvoyWithAverageConfig() throws Exception {
         //load envoy with average concurrency and without limit
         loadEnvoy(quarkusCompositeGWServerUrl.toString() + "api/v1/mesh-test-service-quarkus/sleep?seconds=" + SLEEP_DURATION_SECONDS_STRING, false);
     }
 
     @Test
-    public void testLoadEnvoyWithConcurrency1WithoutConnectionLimit() throws Exception {
+    void testLoadEnvoyWithConcurrency1WithoutConnectionLimit() throws Exception {
         //load envoy with concurrency=1 and without limit
         loadEnvoy(egressGatewayUrl.toString() + "api/v1/mesh-test-service-quarkus/sleep?seconds=" + SLEEP_DURATION_SECONDS_STRING, false);
     }
@@ -94,17 +86,17 @@ public class ThresholdIT {
         for (int i = 0; i < REQUESTS_NUMBER; i++) {
             reqs.add(() -> {
                 try {
-                    URL url = new URL(requestUrl);
+                    URL url = new URI(requestUrl).toURL();
                     HttpURLConnection httpURLConnection1 = (HttpURLConnection) url.openConnection();
                     httpURLConnection1.connect();
                     log.info("response_code:" + httpURLConnection1.getResponseCode());
                     responsesTimes.add(System.currentTimeMillis());
-                } catch (IOException e) {
+                } catch (IOException | URISyntaxException e) {
                     throw new RuntimeException(e);
                 }
             });
         }
-        List<Future<?>> futures = reqs.stream().map(executorService::submit).collect(Collectors.toList());
+        List<? extends Future<?>> futures = reqs.stream().map(executorService::submit).toList();
         for (Future<?> future : futures)
         {
             future.get();
